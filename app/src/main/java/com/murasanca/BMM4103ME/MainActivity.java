f579891665d1d31dacf36fbd8df8074a7c0cbc14
Murat Sancak <murasanca@protonmail.com>
2022-11-19 13:13:58 +0300

    2022.11.19

---
+++ app/src/main/java/com/murasanca/BMM4103ME/MainActivity.java
@@ -0,0 +1,14 @@
+package com.murasanca.BMM4103ME;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

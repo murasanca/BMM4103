bd2dd2a7a7132adad6976255f00bef2b7830d901
Murat Sancak <murasanca@protonmail.com>
2022-11-19 13:52:11 +0300

    2022.11.19

--- app/src/main/java/com/murasanca/BMM4103ME/HomeActivity.java
+++ app/src/main/java/com/murasanca/BMM4103ME/HomeActivity.java
@@ -2,14 +2,12 @@ package com.murasanca.BMM4103ME;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.WindowManager;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +19,6 @@ public class MainActivity extends AppCompatActivity {
         );
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed
-                (
-                        ()->
-                        {
-                            startActivity(new Intent(MainActivity.this,HomeActivity.class));
-                            finish();
-                        },
-                        2000
-                );
+        setContentView(R.layout.activity_home);
     }
 }
